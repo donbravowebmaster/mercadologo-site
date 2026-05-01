2581884dca53bd7ae6cbfcc89cb2970cfca47d56
donbravo1721 <lricardobravo@gmail.com>
2026-05-01 15:13:11 -0600

    Fresh start: minimal Cloudflare Pages setup

--- public/js/main.js
+++
@@ -1,6 +0,0 @@
-console.log('Mercadologo sitio cargado');
-
-document.querySelector('.btn-primary').addEventListener('click', () => {
-  alert('¡Bienvenido! Esta es una demostración.');
-  document.querySelector('#productos').scrollIntoView({ behavior: 'smooth' });
-});
